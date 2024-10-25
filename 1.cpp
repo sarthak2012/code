a717faec1771097ebd82a41b0d060567aa01ff35
Sarthak Srivastava <Sarthaksrivastava5765@gmail.com>
2024-10-25 22:31:52 +0530

    Create 01.cpp

---
+++ 1.cpp
@@ -0,0 +1,64 @@
+// string str; 
+// getline(cin, str); 
+// cout << str; 
+// return 0;
+// // hi raj
+
+// #include<bits/stdc++.h> 
+// using namespace std; 
+// int main(){ 
+// string s = "Striver"; 
+// int lens.size();
+// s[len-1] = 'z'; 
+// cout << s[len]; 
+// return 0; 
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){ 
+//     for(int i =0;i<4;i++){
+//         for(int j =0;j<4;j++){
+//             cout<< "*";
+//             cout<<" ";
+//         }
+//         cout<<endl;
+        
+//     }
+//     return 0;
+    
+// }
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){ 
+//     for(int i =0;i<4;i++){
+//         for(int j =0;j<i;j++){
+//             cout<< "*";
+//             cout<<" ";
+//         }
+//         cout<<endl;
+        
+//     }
+//     return 0;
+    
+// }
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){ 
+    for(int i =1;i<=5;i++){
+        for(int j =1;j<=5-i+1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+        
+    }
+    return 0;
+    
+}
