961f853172e0a6054a22f25152dc639634ad9493
Sarthak Srivastava <Sarthaksrivastava5765@gmail.com>
2024-10-27 11:22:09 +0530

    Create 05.cpp

---
+++ 05.cpp
@@ -0,0 +1,22 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+void print(int i ,int n){
+    //code to print name n times using reccursion
+    if (i>n){
+    return; 
+    // base condition
+    }
+    cout<<i<<endl;
+    print(i+1,n); //reccursive call
+}
+
+int main() {
+    int n;
+    cout<<"enter the number of times you want to print the name"<<endl;
+    cin>>n;
+    cout<<"the number are"<<endl;
+    print(1,n);
+   
+    return 0;
+}
